02f3a1e8a96a6ea291a2c363f29fea632ab01ffe
luis <felipesantosof@gmail.com>
2021-11-29 18:25:17 -0300

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,24 @@
+//let tasklist = ['tomar café','reunião','brancar com o cachorro'];
+
+//tasklist.unshift('acordar');
+
+//tasklist.pop();   remove o ultimo item da lista
+//.shift     remove o primeiro item da lista
+
+//console.log(tasklist);
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1];
+
+//console.log(menuServices);
+
+//let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+//for (let cont = 0; cont < groceryList.length; cont += 1) {
+//    console.log(groceryList[cont]);
+//}
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nome of names){
+    console.log(nome);
+}
